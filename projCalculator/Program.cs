5dd69617830448d7dd1eb9f9b4a6b668bc62ead7
Christ ASSANA <christassana@gmail.com>
2024-06-07 11:11:56 +0200

    Update

--- projCalculator/Program.cs
+++ projCalculator/Program.cs
@@ -9,46 +9,49 @@ class MainProgram
         float result;
         bool calcul = false;
 
-        
-        Console.WriteLine("Quelle est votre premier nombre? ");
-        number1 = float.Parse(Console.ReadLine());
+        while (calcul = true)
+        {
+            Console.WriteLine("Quelle est votre premier nombre? ");
+            number1 = float.Parse(Console.ReadLine());
 
-        Console.WriteLine("Quelle est votre second nombre? ");
-        number2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Quelle est votre second nombre? ");
+            number2 = float.Parse(Console.ReadLine());
 
-        Console.WriteLine("Quelle opération souhaitez vous réaliser entre : addition, soustraction, multiplication, division ou modulo? ");
-        response = Console.ReadLine();
+            Console.WriteLine("Quelle opération souhaitez vous réaliser entre : addition, soustraction, multiplication, division ou modulo? ");
+            response = Console.ReadLine();
 
-        if (response == "addition")
-        {
-            result = add(number1, number2);
-            Console.WriteLine("Votre résultat est : " + result);
-        }
-        else if (response == "soustraction")
-        {
-            result = subtract(number1, number2);
-            Console.WriteLine("Votre résultat est : " + result);
-        }
+            if (response == "addition")
+            {
+                result = add(number1, number2);
+                Console.WriteLine("Votre résultat est : " + result);
+            }
+            else if (response == "soustraction")
+            {
+                result = subtract(number1, number2);
+                Console.WriteLine("Votre résultat est : " + result);
+            }
 
-        else if(response == "multiplication")
-        {
-            result = multiply(number1, number2);
-            Console.WriteLine("Votre résultat est : " + result);
-        }
+            else if (response == "multiplication")
+            {
+                result = multiply(number1, number2);
+                Console.WriteLine("Votre résultat est : " + result);
+            }
 
-        else if (response == "division")
-        {
-            result = divide(number1, number2);
-            Console.WriteLine("Votre résultat est : " + result);
-        }
-        else if(response == "modulo")
-        {
-            result = modulo(number1, number2);
-            Console.WriteLine("Votre résultat est : " + result);
-        }
-        else
-        {
-            Console.WriteLine("Votre réponse n'est pas reconnu. Veuillez recommencer. ");
+            else if (response == "division")
+            {
+                result = divide(number1, number2);
+                Console.WriteLine("Votre résultat est : " + result);
+            }
+            else if (response == "modulo")
+            {
+                result = modulo(number1, number2);
+                Console.WriteLine("Votre résultat est : " + result);
+            }
+            else
+            {
+                Console.WriteLine("Votre réponse n'est pas reconnu. Veuillez recommencer. ");
+            }
+            calcul = true;
         }
     }
 
